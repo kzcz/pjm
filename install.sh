c07508dd819884ecd5d44b3db35bd31134f650fa
Cursed <cursitical@gmail.com>
2024-11-28 23:49:30 -0300

    make the install directory hidden by default

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-read -t 5 -p "Install location --> " -ei "$HOME/pjm" LOCATION
+read -t 5 -p "Install location --> " -ei "$HOME/.pjm" LOCATION
 mkdir -p $LOCATION || { echo "Couldn't create that path."; exit 1; }
 cp ./pjm.sh ./pjm_bash_autocomp.sh $LOCATION || { echo "Error copying files."; exit 2; }
 chmod 0744 $LOCATION/pjm.sh $LOCATION/pjm_bash_autocomp.sh
