a9e1612c82aedfdd8da4fd94050f00f656bef3ab
Cursed <cursitical@gmail.com>
2026-02-23 23:42:18 -0300

    Fixed date in copyright header of pjm.zsh

--- pjm.zsh
+++ pjm.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-# Copyright 2024-(...) Kilzwitch Team
+# Copyright 2026-(...) Kilzwitch Team
 # Owner:        cursitical@gmail.com
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
